f04ce8c740df4e11aa3d34a0a262226868f0c523
Di Mo <dimo@Dis-MacBook-Air.local>
2026-05-29 11:46:19 -0700

    refactor(storyteller): type arena seats wrapper

--- src/components/StorytellerSub/Arena/ArenaSeats.tsx
+++ src/components/StorytellerSub/Arena/ArenaSeats.tsx
@@ -1,13 +1,10 @@
-// @ts-nocheck
-import type { StorytellerSeat } from '../types'
 import type { StorytellerContext } from '../useStoryteller'
-import React from 'react'
 import { ArenaSeat } from './ArenaSeat'
 
 export function ArenaSeats({ ctx, isPortrait }: { ctx: StorytellerContext; isPortrait: boolean }) {
   return (
     <>
-      {ctx.currentDay.seats.map((seat: any, index: number) => (
+      {ctx.currentDay.seats.map((seat, index) => (
         <ArenaSeat ctx={ctx} index={index} key={seat.seat} seat={seat} isPortrait={isPortrait} />
       ))}
     </>
